4b22a8381c469dad82b039eb3c345eb5b94b3c09
Wuan_26092004 <phamhuyen1115204@gmail.com>
2025-12-03 19:10:27 +0700

    maintain lại, tạo dtos

---
+++ SOS.Backend/SOS.Application/DTOs/ReportSosDto/CreateReportRequestDto.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Sos.Application.DTOs.ReportSosDto
+{
+    public record CreateReportRequest(
+        string Phone, 
+        string? Name, 
+        double Lat, 
+        double Lng, 
+        string? Address, 
+        string? Details, 
+        string Level = "critical"
+    );
+}
